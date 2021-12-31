22ef68c8115ba5c46f3c14c4fade4c1b75bc27a6
inseoklee <tjrkd222@gmail.com>
2021-12-31 19:10:53 +0900

    모델링 + 모델 테스트 셋 구현.

---
+++ lib/controller/userController.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:online_counseling_center/model/user.dart';
+
+class UserController extends GetxController{
+  late Rx<User> user;
+
+
+}
---
+++ lib/model/user.dart
@@ -0,0 +1,8 @@
+class User{
+  final String uID;
+  final int age;
+  final String nickName;
+  //TODO: 게시글 리스트, 내 댓글 리스트
+
+  User(this.age, this.nickName, this.uID);
+}
