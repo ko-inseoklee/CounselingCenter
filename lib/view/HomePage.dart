5128187d5fd2af99d213742854202c10218071d7
someng2 <bsm4045@naver.com>
2022-01-05 20:09:53 +0900

    sm [CREATE] matching.dart

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,11 +8,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('메인'),
+      ),
       body: Center(
-        child: Text('급식들과 주전부리'),
+        child: Column(
+          children: [
+            Text('급식들과 주전부리'),
+            TextButton(
+                onPressed: () {
+                  Get.toNamed('/matching');
+                },
+                child: Text('매칭 페이지'))
+          ],
+        ),
       ),
     );
   }
-
 }
-
